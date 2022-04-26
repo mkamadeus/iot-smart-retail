e5c6066f0f67c39216db00071729868284daca15
Samuel <samnap11@gmail.com>
2022-04-27 04:16:47 +0700

    fix: simplify return statement

--- service/entry/status.go
+++ service/entry/status.go
@@ -2,16 +2,13 @@ package entry
 
 import (
 	"context"
-
 	"github.com/go-redis/redis/v8"
+
 	"github.com/google/uuid"
 )
 
 func (s *Service) Check(cardId *uuid.UUID) bool {
 	ctx := context.Background()
 	_, err := s.Cache.Get(ctx, cardId.String()).Result()
-	if err == redis.Nil {
-		return false
-	}
-	return true
+	return err != redis.Nil
 }

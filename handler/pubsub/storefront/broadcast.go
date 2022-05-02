561291d013631d0caca029f8ea78c80bf5dd4ae0
Samuel <samnap11@gmail.com>
2022-05-02 08:18:40 +0700

    feat(utils): add string-byte and byte-string conversion

--- handler/pubsub/storefront/broadcast.go
+++ handler/pubsub/storefront/broadcast.go
@@ -2,9 +2,10 @@ package storefront
 
 import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/mkamadeus/iot-smart-retail/utils"
 )
 
 func (h *Handler) ReceiveMessage(_ mqtt.Client, m mqtt.Message) {
-	msg := string(m.Payload()[:])
+	msg := utils.B2S(m.Payload())
 	h.Service.Notifier <- msg
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+	"unsafe"
+)
 
 // SplitByCharacter splits string by a particular character and returns
 // an array of strings
@@ -8,4 +12,24 @@ func SplitByCharacter(s string, c rune) []string {
 	return strings.FieldsFunc(s, func(r rune) bool {
 		return r == c
 	})
+}
+
+// B2S converts bytes array to string without memory allocation
+//
+// Note: It may break if string and/or slice header changes in the future Go versions
+func B2S(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// S2B converts string to bytes array without memory allocation
+//
+// Note: It may break if string and/or slice header changes in the future Go versions
+func S2B(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	bh.Data = sh.Data
+	return b
 }
